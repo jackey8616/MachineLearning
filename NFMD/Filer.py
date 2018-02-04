449899aea98481513496fe68cb959e31d05661c8
Clooooode <jackey8616@users.noreply.github.com>
2018-02-05 02:23:58 +0800

    Add check duplicate block

--- NFMD/Filer.py
+++ NFMD/Filer.py
@@ -1,4 +1,4 @@
-import os import listdir
+from os import listdir
 from os.path import join, isfile, isdir
 import pandas as pd
 
@@ -19,8 +19,10 @@ class Filer:
                 files.extend(self.getFiles(join(filePath, f)))
         return files
     
-    def concatFiles(self, filesPath):
+    def concatFiles(self, filesPath, dropDupSet=None):
         self.printer.Print('Concating files from path: %s' % filesPath)
+        if dropDupSet is not None:
+            self.printer.Print('Rows will be droped base on duplicate field check: %s' % str(dropDupSet))
         folder = filesPath
         files = self.getFiles(filesPath)
         colDataNames = ['FileID', 'CustomerID', 'QueryTS', 'ProductID']
@@ -29,11 +31,15 @@ class Filer:
         
         _data = pd.DataFrame()
         for filename in files:
-            self.printer.Print('-- (File %d) %s concating...' %  (count + 1, filename), end='')
+            self.printer.Print('- (File %d) %s concating...' %  (count + 1, filename))
             _read = pd.read_csv(filename, names=colDataNames, dtype={'FileID': str, 'CustomerID': str, 'ProductID': str})
             _data = pd.concat([_data, _read], axis=0)
+            if dropDupSet is not None:
+                beforeDropLen = len(_data)
+                _data = _data.drop_duplicates(subset=dropDupSet)
+                self.printer.Print('--- After concate, original length: %d, after drop length: %s.' % (beforeDropLen, len(_data)))
             del _read
-            self.printer.Print('%s down.' % filename, timeInput=False)
+            self.printer.Print('- %s done.' % filename)
             count += 1
         self.printer.Print('Files concated.')
         return _data
